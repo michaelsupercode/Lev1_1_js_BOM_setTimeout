8d961e8b8ad869cc54550f8f3a0f509762b44870
SuperCoder <supercoder@AirvonSerCoder3.fritz.box>
2022-02-26 20:59:35 +0100

    firstcom

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+//Lev1_1//1//
+
+console.log("Start - 3 Sekunden warten..");
+
+setTimeout(function oneOneOne() {
+    console.log("..erledigt, Du hast gerade 3 Sekunden verschwendet..");
+}, 3000);
+
+//Lev1_1//2//
+
+/*function showSec() {
+    let oneOneTwo = new Date()
+    console.log(oneOneTwo.toLocaleTimeString('de-DE'));
+}
+
+let process = setInterval(showSec, 1000);*/
+
+//Lev1_1//3//
+
+/*let countd = 11;
+
+let oneOneThree = setInterval(() => {
+
+    countd--;
+    console.log(countd);
+
+    if (countd === 0) {
+        console.log("..done..");
+        clearInterval(oneOneThree);
+    }
+}, 1000);*/
