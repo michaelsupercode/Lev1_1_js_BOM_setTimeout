e99e2f4d57ab1c9c7b6e8532ff5394bceec63953
Michael Hanel <michaelsupercode@gmail.com>
2023-07-30 11:11:36 +0200

    rev

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,24 +8,24 @@ setTimeout(function oneOneOne() {
 
 //Lev1_1//2//
 
-/*function showSec() {
-    let oneOneTwo = new Date()
-    console.log(oneOneTwo.toLocaleTimeString('de-DE'));
-}
+// function showSec() {
+//    let oneOneTwo = new Date()
+//  console.log(oneOneTwo.toLocaleTimeString('de-DE'));
+// }
 
-let process = setInterval(showSec, 1000);*/
+// let process = setInterval(showSec, 1000);
 
 //Lev1_1//3//
 
-/*let countd = 11;
+// let countd = 11;
 
-let oneOneThree = setInterval(() => {
+// let oneOneThree = setInterval(() => {
 
-    countd--;
-    console.log(countd);
+//     countd--;
+//     console.log(countd);
 
-    if (countd === 0) {
-        console.log("..done..");
-        clearInterval(oneOneThree);
-    }
-}, 1000);*/
+//     if (countd === 0) {
+//         console.log("..done..");
+//         clearInterval(oneOneThree);
+//     }
+// }, 1000);
